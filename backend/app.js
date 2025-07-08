38dfa28a31c4b8103ed67aacbe450e9f7df93493
unknown <cuper.fcb@gmail.com>
2025-07-08 10:49:27 -0600

    swagger

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,22 @@ import cookieParser from "cookie-parser"
 import cors from "cors"
 import clientsRoutes from "./src/routes/clients.routes.js";
 import reservationsRoutes from "./src/routes/reservations.routes.js";
+import swaggerUi from "swagger-ui-express"
+import fs from "fs"
+import path from "path";
 
 const app = express();
 
 app.use(express.json());
 //Que acepte cookies en postman
 app.use(cookieParser());
+
+const swaggerDocument = JSON.parse(
+    fs.readFileSync(path.resolve("./documentation.json"), "utf-8")
+);
+
+
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/clients", clientsRoutes);
 app.use("/reservations", reservationsRoutes);
